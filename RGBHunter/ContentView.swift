f33319e52f62e06798dff107192d8505321e7259
r.gago <rugana90@gmail.com>
2023-12-24 17:31:03 +0100

    Initial Commit

---
+++ RGBHunter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RGBHunter
+//
+//  Created by Rubén Gago on 24/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RGBHunter/RGBHunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBHunterApp.swift
+//  RGBHunter
+//
+//  Created by Rubén Gago on 24/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RGBHunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
